41989c92bf7079cfce25c768a7d55a33b8131667
e1z2x <hc2020@student.ubc.ca>
2022-07-23 11:48:16 -0700

    Completed implentation and test suites for classes in the the model package

--- src/main/model/Media.java
+++ src/main/model/Media.java
@@ -1,13 +1,11 @@
 package model;
 
-import java.util.ArrayList;
-
 // Represents a piece of media having a title, release date, a genre, and a rating
 public class Media {
     private String title;
     private ReleaseDate releaseDate;
     private String genre;
-    private int rating;
+    private double rating;
 
     // REQUIRES: title length > 0 and rating is in [0, 100]
     // EFFECTS: constructs a media object with a title, release date, genre, and rating.
@@ -19,24 +17,31 @@ public class Media {
         this.rating = 0;
     }
 
+    // REQUIRES: rating is in [0, 100]
+    // MODIFIES: this
+    // EFFECTS: sets rating with the user's desired rating input
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
     // EFFECTS: returns the title of the piece of media
     public String getTitle() {
-        return ""; // stub
+        return this.title;
     }
 
     // EFFECTS: returns the release date of the piece of media
     public ReleaseDate getReleaseDate() {
-        return new ReleaseDate(1,2,3); // stub
+        return this.releaseDate;
     }
 
     // EFFECTS: returns the genre of the piece of media
     public String getGenre() {
-        return ""; // stub
+        return this.genre;
     }
 
     // EFFECTS: returns the rating of the piece of media
-    public int getRating() {
-        return 0; // stub
+    public double getRating() {
+        return rating;
     }
 
 }
--- src/main/model/ReleaseDate.java
+++ src/main/model/ReleaseDate.java
@@ -5,14 +5,28 @@ public class ReleaseDate {
     private int year;
     private int month;
     private int day;
-    private static final int PRESENT_YEAR = 2022;
 
-    // REQUIRES: year is [1700, PRESENT_YEAR], month is [1, 12], day is [1, 31]
-    // EFFECTS: Instantiates a ReleaseDate object with a year, month and day
+    // REQUIRES: year >= 1700, month is [1, 12], day is [1, 31]
+    // EFFECTS: instantiates a ReleaseDate object with a year, month and day
     public ReleaseDate(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
+    }
+
+    // EFFECTS: returns the year
+    public int getYear() {
+        return this.year;
+    }
+
+    // EFFECTS: returns the month
+    public int getMonth() {
+        return this.month;
+    }
+
+    // EFFECTS: returns the day
+    public int getDay() {
+        return this.day;
     }
 
 }
