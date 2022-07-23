0120f7c5cf7a336dcd4631cc704a7cbdc07b31aa
e1z2x <hc2020@student.ubc.ca>
2022-07-22 20:08:26 -0700

    Added user stories and added the method signatures for simple getters and setters for the Movie superClass

--- src/main/model/Episode.java
+++ src/main/model/Episode.java
@@ -1,11 +1,11 @@
 package model;
 
-import java.util.List;
+import java.util.ArrayList;
 
 // Represents an episode having a title, release date, list of characters, a genre, and a rating
-public class Episode extends Movie{
+public class Episode extends Movie {
 
-    public Episode(String title, ReleaseDate releaseDate, List<Character> characters, String genre, int rating) {
+    public Episode(String title, ReleaseDate releaseDate, ArrayList<Character> characters, String genre, int rating) {
         super(title, releaseDate, characters, genre, rating);
     }
 }
--- src/main/model/Movie.java
+++ src/main/model/Movie.java
@@ -1,23 +1,48 @@
 package model;
 
-import java.util.List;
+import java.util.ArrayList;
 
 // Represents a movie having a movie title, release date, list of characters, a genre, and a rating
 public class Movie {
     private String title;
     private ReleaseDate releaseDate;
-    private List<Character> characters;
+    private ArrayList<Character> characters;
     private String genre;
     private int rating;
 
     // REQUIRES: title length > 0,
     // EFFECTS: constructs a movie object with a title, release date, list of characters, genre, and rating
-    public Movie(String title, ReleaseDate releaseDate, List<Character> characters, String genre, int rating) {
+    public Movie(String title, ReleaseDate releaseDate, ArrayList<Character> characters, String genre, int rating) {
         this.title = title;
         this.releaseDate = releaseDate;
         this.characters = characters;
         this.genre = genre;
         this.rating = rating;
+    }
+
+    // EFFECTS: returns the title of the movie
+    public String getTitle() {
+        return "";// stub
+    }
+
+    // EFFECTS: returns the release date of the movie
+    public ReleaseDate getReleaseDate() {
+        return new ReleaseDate(1,2,3);// stub
+    }
+
+    // EFFECTS: returns the list of characters in the movie
+    public ArrayList<Character> getCharacters() {
+        return new ArrayList<>();// stub
+    }
+
+    // EFFECTS: returns the genre of the movie
+    public String getGenre() {
+        return "";// stub
+    }
+
+    // EFFECTS: returns the rating of the movie
+    public int getRating() {
+        return 0;// stub
     }
 
 }
